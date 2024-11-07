ce384a7d7ecdf6c8229f4ff5583b1ea554489ae4
star.stalker9160 <abyazjschool@gmail.com>
2024-11-07 10:33:57 +0400

    got lazy working

---
+++ lua/lazy/lazy.lua
@@ -0,0 +1,13 @@
+local lazyPath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazyPath) then
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim", "--branch=stable", lazyPath
+	})
+end
+
+vim.opt.rtp:prepend(lazyPath)
+
+require("lazy").setup({
+	
+})
