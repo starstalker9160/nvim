3fb32c85a69822aeebddaebab33b2af293cf0d98
starstalker9160 <abyazjschool@gmail.com>
2025-12-25 20:32:33 +0400

    a few much needed updates

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,8 +6,18 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
+-- search clearing
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
+
 -- split windows stuff
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
-keymap.set("n", "<leaders>sh", "<C-w>h", { desc = "Split horizontally" })
+keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Split horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close active split" })
+
+-- tab stuff
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab"})
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab"})
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab (right)"})
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab (left)"})
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab"})
