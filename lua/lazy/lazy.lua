86813d35f4037974f4aa67d945c24c2d801ad610
starstalker9160 <abyazjschool@gmail.com>
2026-01-01 20:14:47 +0400

    fixed a thing where it'd break every install

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -2,7 +2,7 @@ local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazyPath) then
 	vim.fn.system({
-		"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim", "--branch=stable", lazyPath
+		"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazyPath
 	})
 end
 
