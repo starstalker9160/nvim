d23f6698be9cf74c6883d5d8843637fcfac43546
starstalker9160 <abyazjschool@gmail.com>
2026-01-11 06:13:24 +0400

    update colours

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,13 +1,33 @@
 return {
-	"folke/tokyonight.nvim",
-	priority = 1000,
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			on_colors = function(colors)
-				colors.bg_highlight = "#143652"
-			end
-		})
-		vim.cmd("colorscheme tokyonight")
-	end
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        config = function()
+            local bg_dark = "#011423"
+            local bg_highlight = "#143652"
+            local bg_visual = "#275378"
+            local fg = "#CBE0F0"
+            local fg_dark = "#B4D0E9"
+            local fg_gutter = "#627E97"
+            local border = "#547998"
+
+            require("tokyonight").setup({
+                style = "night",
+                on_colors = function(colors)
+                    colors.bg_highlight = bg_highlight
+                    colors.bg_popup = bg_dark
+                    colors.bg_statusline = bg_dark
+                    colors.bg_visual = bg_visual
+                    colors.border = border
+                    colors.fg = fg
+                    colors.fg_dark = fg_dark
+                    colors.fg_float = fg
+                    colors.fg_gutter = fg_gutter
+                    colors.fg_sidebar = fg_dark
+                end,
+            })
+
+            vim.cmd([[colorscheme tokyonight]])
+        end
+    }
 }
