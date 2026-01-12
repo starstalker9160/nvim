b5b342dcb7163db96afed6cfd5cb5089c853bc4d
starstalker9160 <abyazjschool@gmail.com>
2026-01-12 20:14:52 +0400

    idk why but sure!

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -4,6 +4,15 @@ if not vim.loop.fs_stat(lazyPath) then
 	vim.fn.system({
 		"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazyPath
 	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 vim.opt.rtp:prepend(lazyPath)
